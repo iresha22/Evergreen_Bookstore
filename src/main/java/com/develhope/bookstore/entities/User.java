1b92f4efe9c7e8f4897061b6b190f19b320e5c1d
iresha warnakulasuriya <dilhanimadu2002@gmail.com>
2024-11-03 15:04:57 +0100

    created custom query for AuthorRepository

--- src/main/java/com/develhope/bookstore/entities/User.java
+++ src/main/java/com/develhope/bookstore/entities/User.java
@@ -9,6 +9,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    @Column(name = "is_active")
     private boolean isActive = true;
     @Column(unique = true)
     private String email;
--- src/main/java/com/develhope/bookstore/repositories/AuthorRepository.java
+++ src/main/java/com/develhope/bookstore/repositories/AuthorRepository.java
@@ -2,8 +2,19 @@ package com.develhope.bookstore.repositories;
 
 import com.develhope.bookstore.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author,Long> {
+
+    // custom query for elimination logic
+    // update the table and make the isActive false
+    // query to findactivebyid
+    // select from the table author where it is active and the id is equal to the parameter
+    @Query(value = "select * from author where is_active = true and id = :id", nativeQuery = true)
+    Author findActiveById(@Param("id") Long id);
+    // find all active by id
+    // select all the authors where isactive = true
 }
